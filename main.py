cc88c40384438e4f137e55a202c0eddfc3cc7060
vladislav ziplant <v.sinitsyn@open-bs.ru>
2025-04-11 22:42:37 +0300

    добавил сохранение фото с кодом фото

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from photo_preview import photo_preview
 
 async def main():
     url = input("Введите URL: ")
+    path_directory = input("Введите путь к директории: ")
+    #
+
     # url = "https://www.gettyimages.es/search/2/image?family=editorial&phrase=1993%20julia%20roberts%20Audrey%20Hepburn&sort=best"
+    # path_directory = "/home/vlad/Документы/Vlad/Alex_Photo"
+
     url_preview = url
     url_osn = url
-    path_directory = input("Введите путь к директории: ")
-    # path_directory = "/home/vlad/Документы/Vlad/Alex_Photo"
+
+
 
     await photo_osn(url_osn,path_directory)
     # await photo_preview(url_preview,path_directory)
